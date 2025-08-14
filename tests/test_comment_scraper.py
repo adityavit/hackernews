4f00fe0dfef4b300bb5583e6ca50915387a8e008
Aditya Bhatia <mail@adityabhatia.com>
2025-08-14 10:25:34 -0700

    feat: add comments scraper module + API endpoint; CLI for comment scraping; UI + API run targets; CORS config; integration-style tests; README updates; .gitignore and Make improvements

--- tests/test_comment_scraper.py
+++ tests/test_comment_scraper.py
@@ -95,3 +95,65 @@ def test_fetch_story_comments_respects_depth_and_limit(mock_get):
     assert len(limited) == 1
 
 
+@patch('requests.get')
+def test_fetch_story_comments_realistic_html(mock_get):
+    realistic_item_html = """
+    <!doctype html>
+    <html><body>
+      <table>
+       <tr class='athing' id='100001'>
+         <td class='title'><span class='titleline'><a href='https://example.com/a'>A title</a></span></td>
+       </tr>
+       <tr>
+         <td class='subtext'>
+           <span class='score' id='score_100001'>123 points</span>
+           by <a href='user?id=alice' class='hnuser'>alice</a>
+           <span class='age'><a href='item?id=100001'>2 hours ago</a></span>
+           | <a href='item?id=100001'>42 comments</a>
+         </td>
+       </tr>
+       <tr class='athing comtr' id='c1'>
+         <td class='ind'><img src='s.gif' width='0'></td>
+         <td class='default'>
+           <div class='comment'><span class='commtext c00'>Top level comment</span></div>
+           <div class='comhead'>
+             <a class='hnuser'>user1</a>
+             <span class='age'><a href='item?id=c1'>2 hours ago</a></span>
+             | <a href='item?id=p1'>parent</a>
+           </div>
+         </td>
+       </tr>
+       <tr class='athing comtr' id='c2'>
+         <td class='ind'><img src='s.gif' width='40'></td>
+         <td class='default'>
+           <div class='comment'><span class='commtext c00'>Nested comment</span></div>
+           <div class='comhead'>
+             <a class='hnuser'>user2</a>
+             <span class='age'><a href='item?id=c2'>30 minutes ago</a></span>
+             | <a href='item?id=c1'>parent</a>
+           </div>
+         </td>
+       </tr>
+      </table>
+      </body></html>
+    """
+
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.content = realistic_item_html
+    mock_get.return_value = mock_response
+
+    comments = fetch_story_comments('100001', max_depth=2)
+    assert len(comments) == 2
+    assert comments[0]['id'] == 'c1'
+    assert comments[0]['author'] == 'user1'
+    assert comments[0]['depth'] == 0
+    assert comments[0]['text'] == 'Top level comment'
+    assert comments[0]['parent_id'] == 'p1'
+
+    assert comments[1]['id'] == 'c2'
+    assert comments[1]['author'] == 'user2'
+    assert comments[1]['depth'] == 1
+    assert comments[1]['text'] == 'Nested comment'
+    assert comments[1]['parent_id'] == 'c1'
+
